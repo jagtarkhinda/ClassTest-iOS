9b9f7909e0849e0ec1e0b09d790b0ebe6df0719e
MacStudent <macstudent@130-IMAC-11.local>
2019-06-19 10:02:38 -0400

    some code

--- iosGame/GameScene.swift
+++ iosGame/GameScene.swift
@@ -11,10 +11,34 @@ import GameplayKit
 
 class GameScene: SKScene {
     
-  
+    // keep track of all the lemmings
+    var leming:[SKShapeNode] = []
     
     override func didMove(to view: SKView) {
+        // Add borders around the scene
+        self.physicsBody = SKPhysicsBody(edgeLoopFrom: self.frame)
         
+    }
+    
+    func makeleming() {
+        // lets add some lemings
+        let leming = SKShapeNode(ellipseIn: CGRect(x: 690, y: 1090, width: 100, height: 100))
+        leming.physicsBody
+        
+//
+//        // generate a random (x,y) for the cat
+//        let randX = Int(CGFloat(arc4random_uniform(UInt32(self.size.width-400))))
+//        let randY = Int(CGFloat(arc4random_uniform(UInt32(self.size.height-400))))
+        
+       // cat.position = CGPoint(x:randX, y:randY)
+        
+        // add the cat to the scene
+        addChild(cat)
+        
+        // add the cat to the cats array
+        self.cats.append(cat)
+        
+        print("Where is cat? \(randX), \(randY)")
     }
     
     
